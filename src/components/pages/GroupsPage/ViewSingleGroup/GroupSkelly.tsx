40765d570ecba0ca01bbe2ede258dbffaf2f1978
MaxRubel <maxrubel1@gmail.com>
2024-10-14 20:15:56 -0500

    can remove user from group

--- src/components/pages/GroupsPage/ViewSingleGroup/GroupSkelly.tsx
+++ src/components/pages/GroupsPage/ViewSingleGroup/GroupSkelly.tsx
@@ -4,25 +4,35 @@ export default function GroupSkelly() {
   return (
     <div>
       <Skeleton
-        style={{ marginBottom: '.2em' }}
-        className="w-[250px] h-[40px] rounded-full bottom-4" />
+        style={{ marginBottom: ".2em" }}
+        className="w-[250px] h-[40px] rounded-full bottom-4"
+      />
       <Skeleton
-        style={{ marginBottom: '3em' }}
-        className="w-[380px] h-[20px] rounded-full" />
+        style={{ marginBottom: "3em" }}
+        className="w-[380px] h-[20px] rounded-full"
+      />
       <Skeleton
-        style={{ marginBottom: '4em' }}
-        className="w-[380px] h-[220px] " />
-      <div style={{ display: 'flex', gap: '1em' }}>
+        style={{ marginBottom: "3em" }}
+        className="w-[380px] h-[60px]"
+      />
+      <Skeleton
+        style={{ marginBottom: "4em" }}
+        className="w-[380px] h-[220px] "
+      />
+      <div style={{ display: "flex", gap: "1em" }}>
         <Skeleton
-          style={{ marginBottom: '1em' }}
-          className="w-[100px] h-[44px] " />
+          style={{ marginBottom: "1em" }}
+          className="w-[100px] h-[44px] "
+        />
         <Skeleton
-          style={{ marginBottom: '4em' }}
-          className="w-[100px] h-[44px] " />
+          style={{ marginBottom: "4em" }}
+          className="w-[100px] h-[44px] "
+        />
       </div>
       <Skeleton
-        style={{ marginBottom: '1em' }}
-        className="w-[100px] h-[44px] " />
+        style={{ marginBottom: "1em" }}
+        className="w-[100px] h-[44px] "
+      />
     </div>
-  )
+  );
 }
