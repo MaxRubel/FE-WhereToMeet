e8d96e3ba81d8850c55126d759161e02274a7ef5
frank <frankcampos85@gmail.com>
2024-10-17 20:30:36 -0500

    Add console logs for user ID and groups in ViewGroups component

--- src/components/pages/GroupsPage/ViewGroups/ViewGroups.tsx
+++ src/components/pages/GroupsPage/ViewGroups/ViewGroups.tsx
@@ -6,6 +6,8 @@ import SmallGroupCard from "../Components/SmallGroupCard";
 export default function ViewGroups() {
   const { user } = useAuth();
   const { data: groups, isLoading } = useGetUserGroups(user._id);
+  console.log("this is my user_id", user.id);
+  console.log("this is my groups", groups)
 
   if (isLoading) {
     return (
