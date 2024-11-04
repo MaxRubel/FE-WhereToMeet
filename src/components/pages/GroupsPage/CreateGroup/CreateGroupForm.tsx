c0a5142fdf13dfa69d7d22c3629d56df21622fc7
frank <frankcampos85@gmail.com>
2024-11-03 22:31:04 -0600

    add email field to user object in CreateGroupForm

--- src/components/pages/GroupsPage/CreateGroup/CreateGroupForm.tsx
+++ src/components/pages/GroupsPage/CreateGroup/CreateGroupForm.tsx
@@ -61,6 +61,7 @@ export default function CreateGroupForm({ setIsViewing }: props) {
         {
           _id: user._id,
           joined: new Date(),
+          email:user.email
         },
       ],
     };
